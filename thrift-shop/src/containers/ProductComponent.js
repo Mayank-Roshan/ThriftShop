9977c34c1b9cfedce7579dbf03157690d5b1acb3
Mayank Roshan aka Thrill.Er <32671184+Mayank-Roshan@users.noreply.github.com>
2021-05-29 19:45:33 +0530

    Mayank - Added definitions to Containers

---
+++ thrift-shop/src/containers/ProductComponent.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+
+function ProductComponent() {
+    const products = useSelector((state) => state.allProducts.products);
+    const {id,title} = products[0];
+
+    console.log(products);
+    console.log("Product Component function Reached")
+
+    return (
+        <div className="four column wide">
+            <div className="ui link card">
+                <div className="image">
+
+                </div>
+                <div className="content">
+                    <div className="header">{title}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductComponent
